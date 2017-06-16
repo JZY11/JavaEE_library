0140e8f2c006860c4919144777c52c3e457f983f
Tony.Jaa <1291813139@qq.com>
2017-06-16 11:01:12 +0800

    think second

--- src/main/java/demo/util/Db.java
+++ src/main/java/demo/util/Db.java
@@ -1,9 +1,48 @@
 package demo.util;
 
+import com.mysql.jdbc.Driver;
+
+import javax.servlet.http.HttpServletRequest;
+import java.sql.*;
+
 /**
  * Created by zhenya.1291813139.com
  * on 2017/6/16.
  * JavaEE_library.
  */
 public class Db {
+    private static final String URL = "jdbc:mysql///?user=root&password=system"
+    public static Connection getconnection(){
+        try {
+            new Driver();
+            return DriverManager.getConnection(URL);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void close(ResultSet resultSet, PreparedStatement preparedStatement,Connection connection){
+        if (resultSet != null) {
+            try {
+                resultSet.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if (preparedStatement != null) {
+            try {
+                preparedStatement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
