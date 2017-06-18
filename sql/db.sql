4f1bc30d5e219529158b1d4cf2823c0ff9249f3a
Tony.Jaa <1291813139@qq.com>
2017-06-19 03:05:08 +0800

    think second

--- sql/db.sql
+++ sql/db.sql
@@ -12,5 +12,20 @@ CREATE TABLE db_javaee_library.user (
 
 INSERT INTO db_javaee_library.user VALUE (null,'admin','123','管理员');
 
+
+DROP TABLE IF EXISTS db_javaee_library.book;
+CREATE TABLE db_javaee_library.book(
+  id INT AUTO_INCREMENT PRIMARY KEY COMMENT 'ID PK',
+  title VARCHAR(255) NOT NULL COMMENT '书名',
+  author VARCHAR(255) NOT NULL COMMENT '作者',
+  pub VARCHAR(255) NOT NULL COMMENT '出版社',
+  time DATE NOT NULL COMMENT '出版时间',
+  price DECIMAL(8,2) NOT NULL COMMENT '定价',
+  amount INT NOT NULL COMMENT'数量'
+)COMMENT '图书表';
+
 SELECT *
 FROM db_javaee_library.user;
+
+SELECT *
+FROM db_javaee_library.book;
