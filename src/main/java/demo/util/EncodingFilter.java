e0e3a251de80e71a8a78151259d089f749f00b22
Tony.Jaa <1291813139@qq.com>
2017-06-19 02:16:34 +0800

    think second

--- src/main/java/demo/util/EncodingFilter.java
+++ src/main/java/demo/util/EncodingFilter.java
@@ -1,9 +1,27 @@
 package demo.util;
 
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
 /**
  * Created by zhenya.1291813139.com
  * on 2017/6/19.
  * JavaEE_library.
  */
-public class EncodingFilter {
+@WebFilter(urlPatterns = "/*")
+public class EncodingFilter implements Filter{
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        chain.doFilter(request,response);
+    }
+
+    public void destroy() {
+
+    }
 }
